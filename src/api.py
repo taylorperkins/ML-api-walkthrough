0692437f42b58343b496c6aaae542aa9941c7a95
Taylor Perkins <taylorperkins.dev@gmail.com>
2019-05-17 15:10:39 -0500

    Added in a request and response schema, validation around the predict endpoint, and overall cvleaned up the api.py

--- src/api.py
+++ src/api.py
@@ -1,11 +1,13 @@
 import yaml
-import os
 
 import numpy as np
 
-from sklearn.externals import joblib
-
 from flask_api import FlaskAPI, status
+from flask import request
+
+from api_models import PredictRequestSchema, PredictResponseSchema
+
+from utils import validate_request_data, validate_response_data, load_labels, load_model
 
 
 app = FlaskAPI(__name__)
@@ -17,50 +19,28 @@ def health():
 
 
 @app.route('/predict', methods=['POST'])
+@validate_request_data(PredictRequestSchema)
+@validate_response_data(PredictResponseSchema)
 def predict():
-    # TODO: actually implement the dynamic prediction from the request.
-    # TODO: Just bringing this over from the sample_prediction.py for now.
-    sepal_length = 1.0
-    sepal_width = 1.6
-    petal_length = 1.1
-    petal_width = 1.5
-    petal_width_cubed = np.power(petal_width, 3)
-
+    # since we have some validation checks..
+    # it should be totally fine to access the data from the request directly
     single_row_record = np.array(
-        [sepal_length, sepal_width, petal_length, petal_width, petal_width_cubed]
+        [
+            request.data['SepalLength'],
+            request.data['SepalWidth'],
+            request.data['PetalLength'],
+            request.data['PetalWidth'],
+            np.power(request.data['PetalWidth'], 3)
+        ]
     ).reshape(1, -1)
 
     prediction = MODEL.predict(single_row_record)
     predicted_flower_species = FLOWER_SPECIES_NAMES[prediction]
 
-    print(predicted_flower_species[0])
-
-    return '', status.HTTP_200_OK
-
-
-def _load_model(config):
-    """Helper function to hold all info regarding loading in the model"""
-    model_objects_folder = config["Model"]["Objects"]["FolderName"]
-    pickeled_model_file_name = config["Model"]["Objects"]["ModelFileName"]
-
-    model_location = os.path.join(model_objects_folder, pickeled_model_file_name)
-
-    # Load the model from the file
-    return joblib.load(model_location)
-
-
-def _load_labels(config):
-    """Helper function to hold all info regarding loading in the labels"""
-    model_objects_folder = config["Model"]["Objects"]["FolderName"]
-    target_class_names_file_name = config["Model"]["Objects"][
-        "TargetClassLabelNamesFileName"
-    ]
-
-    flower_class_names_location = os.path.join(
-        model_objects_folder, target_class_names_file_name
+    return (
+        {'FlowerID': request.data['FlowerID'], 'Species': predicted_flower_species[0]},
+        status.HTTP_200_OK
     )
-
-    return np.load(flower_class_names_location)
 
 
 if __name__ == '__main__':
@@ -70,11 +50,11 @@ if __name__ == '__main__':
         model_config = yaml.safe_load(f)
 
     # Load the model from the file based on the config
-    MODEL = _load_model(config=model_config)
+    MODEL = load_model(config=model_config)
     app.logger.debug('Model successfully loaded into env')
 
     # Load in the labels from the file based on the config
-    FLOWER_SPECIES_NAMES = _load_labels(config=model_config)
+    FLOWER_SPECIES_NAMES = load_labels(config=model_config)
     app.logger.debug('Labels successfully loaded into env')
 
     app.run()
